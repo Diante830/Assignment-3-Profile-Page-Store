bd8fbcc60cc8b3dbd06bd2ab2baef4aa4ce03d82
Diante830 <100953671@georgebrown.ca>
2024-12-10 17:45:58 +0000

    Image path

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "/Assignment-3-Profile-Page-Store",
+  output: "export",
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
