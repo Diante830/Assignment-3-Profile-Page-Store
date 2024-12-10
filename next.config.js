51eced8e8f56911b6dab4871df8cbe22c295ac7f
Diante830 <100953671@georgebrown.ca>
2024-12-10 17:47:56 +0000

    Start from scratch --- workflow

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/Assignment-3-Profile-Page-Store",
-  output: "export",
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
